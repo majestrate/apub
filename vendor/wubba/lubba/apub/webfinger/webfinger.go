1a50ac67384aa660112b3fd0fca29442f478d405
Jeff Becker <jeff@i2p.rocks>
2018-03-21 09:18:38 -0400

    make it compile

--- vendor/wubba/lubba/apub/webfinger/webfinger.go
+++ vendor/wubba/lubba/apub/webfinger/webfinger.go
@@ -1,9 +1,13 @@
 package webfinger
 
 import (
+	"net/http"
 	"wubba/lubba/apub"
 )
 
 type WebFinger struct {
 	Finder apub.UserFinder
+}
+
+func (w *WebFinger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }

6a5da89bfbdce416fef4f5cbfd0f920e1de2a6cc
Jeff Becker <jeff@i2p.rocks>
2018-03-25 12:22:00 -0400

    more

--- vendor/wubba/lubba/apub/model/person.go
+++ vendor/wubba/lubba/apub/model/person.go
@@ -15,6 +15,7 @@ type apPerson struct {
 	Context   string    `json:"@context"`
 	Type      string    `json:"type"`
 	Name      string    `json:"name"`
+	ID        string    `json:"id"`
 	URL       string    `json:"url"`
 	PublicKey PublicKey `json:"publicKey"`
 }
@@ -25,10 +26,22 @@ func (p Person) MarshalJSON() ([]byte, error) {
 		Type:    PersonType,
 		Name:    p.Name,
 		URL:     p.URL,
+		ID:      p.URL,
 		PublicKey: PublicKey{
 			ID:    p.URL + "#main-key",
 			Owner: p.URL,
 			Key:   p.PublicKey,
 		},
 	})
+}
+
+func (p *Person) UnmarshalJSON(data []byte) error {
+	var ap apPerson
+	err := json.Unmarshal(data, &ap)
+	if err == nil {
+		p.URL = ap.ID
+		p.Name = ap.Name
+		p.PublicKey = ap.PublicKey.Key
+	}
+	return err
 }
